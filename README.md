c454fd98aba899ce348621df6a9660f02dbcbbb5
Alexey Svetliakov <asvetliakov@teledini.com>
2018-11-03 21:00:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# babel-compile-on-save
+
+## Features
+
+* Automatically transpile file by babel on save
+* Experimental: Fast-emitting TS declaration for saved file (250-400ms to produce declaration)
+* Use either project's @babel/core or typescript (for declaration emit) or bundled ones (if not found)
+
+## Requirements
+
+* Babel 7.1+ (for ```upward``` rootMode)
+
+## Extension Settings
+
+This extension contributes the following settings:
+
+* `babel-compile-on-save.include`: List of source file globs to transpile through babel. Something like ```[ "src/**/*.ts", "src/**/*.js" ]```. Default [].
+* `babel-compile-on-save.outDir`: Output directory for transpiled files. E.g. ```build``` or similar. Setting to ```""``` will place transpiled files next to the source file (Beware: may overwrite sourcefile if they have same extension, i.e. ```file.js -> file.js```). Default ```build```.
+* `babel-compile-on-save.srcDir`: Your src root directory. Used to calculate correct sourceFileName mapping from result sourcemap. Default ```src```.
+* `babel-compile-on-save.outExt`: Output filename extension. Default ```.js```. Possible choices: ```.mjs``` or ```.jsx``` (for react-native)
+* `babel-compile-on-save.emitTSDeclaration`: Emit TS declaration for transpiled file. Uses project's tsconfig.json for output paths. Ignores any errors. Suggested use is with project-wide ```tsc -w --noEmit``` background task which will produce errors.  Default ```false```.
+
+## Sourcemaps
+Sourcemap will be written when detected. Set ```sourceMap: true``` (or ```"inline"```) in ```babel.config.js``` or whatever you use for babel config file.
+
+## emitTSDeclaration
+This feature mainly is to support TS monorepo scenarios compiled through babel and @babel/preset-typescript. With ```--emitDeclarationOnly``` the incremental compilation of typescript takes around ~6-10 seconds to finish on medium (or greater) sized projects. This is trying to workaround it by creating TS program with only one file and producing declaration for it only. The result is 250-400ms vs 6-10 seconds. Any errors will be ignored, so you need to have separate reporting channel: ```tsc -w --noEmit``` is pretty good example and fast.
---
+++ src/extension.ts
@@ -0,0 +1,176 @@
+import * as vscode from 'vscode';
+import micromatch from "micromatch";
+import path from "path";
+import makeDir from "make-dir";
+import fs from "fs";
+import { promisify } from "util";
+
+const promisedWriteFile = promisify(fs.writeFile);
+
+const parseConfigHost: import("typescript").ParseConfigFileHost = {
+    fileExists: fs.existsSync,
+    getCurrentDirectory: () => __dirname,
+    readFile: path => fs.readFileSync(path, { encoding: "utf8" }),
+    onUnRecoverableConfigFileDiagnostic: diag => {
+        throw new Error("Error when parsing TS config file: " + diag.messageText.toString());
+    },
+    // we don't need any directory reading
+    readDirectory: () => [],
+    useCaseSensitiveFileNames: true,
+};
+
+
+/**
+ * Compile TS declaration. Creates program only with single sourcefile and tries to get the declaration for it
+ * @param filePath File path
+ */
+async function compileTypescript(filePath: string): Promise<void> {
+    if (!filePath.endsWith(".ts") && !filePath.endsWith(".tsx")) {
+        return;
+    }
+    let tsPath: string;
+    try {
+        tsPath = require.resolve("typescript", {
+            paths: [
+                path.dirname(filePath),
+                __dirname,
+            ],
+        });
+    } catch {
+        throw new Error("Unable to instantiate typescript");
+    }
+    const ts: typeof import("typescript") = require(tsPath);
+    const configPath = ts.findConfigFile(filePath, parseConfigHost.fileExists);
+    if (!configPath) {
+        return;
+    }
+    const parsedConfig = ts.getParsedCommandLineOfConfigFile(
+        configPath,
+        {
+            declaration: true,
+            emitDeclarationOnly: true,
+            composite: false,
+            skipLibCheck: true,
+            noEmitHelpers: true,
+            target: ts.ScriptTarget.ESNext,
+            module: ts.ModuleKind.ESNext,
+            noEmit: false,
+            // 2-2.5x emitting faster 250-300ms vs 600-700ms
+            noResolve: true,
+            // skip any errors since with noResolve it will be a bunch of them
+            noEmitOnError: false,
+        },
+        parseConfigHost,
+    );
+    if (!parsedConfig) {
+        throw new Error(`Unable to parse TS config ${configPath}`);
+    }
+    const program = ts.createProgram({
+        options: parsedConfig.options,
+        rootNames: [filePath],
+    });
+    program.emit();
+}
+
+const SOURCEMAP_REGEX = /^\s*\/\/#\s*sourceMappingURL/m;
+
+/**
+ * Compiles file through babel
+ * @param filePath Input file path
+ * @param outputPath Output file path
+ */
+async function compileBabel(filePath: string, outputPath: string): Promise<void> {
+    const sourceFileName = path.relative(path.dirname(outputPath), filePath);
+    // require @babel/core
+    let babelPath: string;
+    try {
+        babelPath = require.resolve("@babel/core", {
+            paths: [
+                path.dirname(filePath),
+                __dirname,
+            ]
+        });
+    } catch {
+        throw new Error("Unable to instantiate @babel/core");
+    }
+    const babel: typeof import("@babel/core") = require(babelPath);
+    const res = await babel.transformFileAsync(filePath, {
+        root: filePath,
+        rootMode: "upward",
+        sourceFileName,
+    } as any);
+    if (!res || !res.code) {
+        throw new Error(`Got empty output when transipling the file: ${filePath}`);
+    }
+    const outputMapPath = outputPath + ".map";
+    let { code, map } = res;
+    // make sure that output directory exists
+    await makeDir(path.dirname(outputPath));
+    // add sourcemapping url if there is no sourcemap in the code
+    const hasSourceMapping = SOURCEMAP_REGEX.test(code);
+    if (!hasSourceMapping && map) {
+        code += `\n//# sourceMappingURL=${path.basename(outputMapPath)}`;
+    }
+    await Promise.all([
+        promisedWriteFile(outputPath, code, { encoding: "utf8" }),
+        map ? promisedWriteFile(outputMapPath, JSON.stringify(map), { encoding: "utf8" }) : Promise.resolve(),
+    ]);
+}
+
+async function saveListener(e: vscode.TextDocument, output: vscode.OutputChannel): Promise<void> {
+    const workspaceFolder = vscode.workspace.getWorkspaceFolder(e.uri);
+    if (!workspaceFolder || workspaceFolder.uri.scheme !== "file") {
+        return;
+    }
+    const workspacePath = workspaceFolder.uri.fsPath;
+    const config = vscode.workspace.getConfiguration("babel-compile-on-save", e.uri);
+    const fileName = e.fileName;
+    if (!config || !fileName) {
+        return;
+    }
+    const includeGlobs: string[] = config.get("include", []).map(g => `${workspacePath}/${g}`);
+    // micromatch supports glob array, bad typings
+    if (!micromatch.isMatch(fileName, includeGlobs as any)) {
+        return;
+    }
+    // always skip d.ts files
+    if (fileName.endsWith(".d.ts")) {
+        return;
+    }
+    const outDir: string = config.get("outDir", "");
+    const srcDir: string = config.get("srcDir", "");
+    const outExt: string = config.get("outExt", ".js");
+    const emitTSDeclaration = config.get("emitTSDeclaration", false);
+    // const outFilePath = path.resolve(workspacePath, outDir, vscode.workspace.asRelativePath(fileName, false));
+    const outFilePath = path.resolve(workspacePath, outDir, path.relative(path.join(workspacePath, srcDir), fileName));
+    const outFilePathJs = path.join(path.dirname(outFilePath), path.basename(outFilePath, path.extname(outFilePath)) + outExt);
+    output.appendLine(`Compiling file: ${fileName} for workspace: ${workspacePath}, output file: ${outFilePathJs}`);
+
+    try {
+        const babelTime = new Date().getTime();
+        await compileBabel(fileName, outFilePathJs);
+        output.appendLine(`Babel compilation took: ${new Date().getTime() - babelTime}ms`);
+    } catch (e) {
+        output.appendLine(`Unable to transpile file: ${e.message}`);
+        return;
+    }
+    if (emitTSDeclaration) {
+        try {
+            const tsTime = new Date().getTime();
+            compileTypescript(fileName);
+            output.appendLine(`TS compilation took: ${new Date().getTime() - tsTime}ms`);
+        } catch (e) {
+            output.appendLine(`Unable to produce TS declaration: ${e.message}`);
+        }
+    }
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    const channel = vscode.window.createOutputChannel("Babel Compile On Save");
+    const disposable = vscode.workspace.onDidSaveTextDocument(e => saveListener(e, channel));
+    context.subscriptions.push(disposable, channel);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {
+}
